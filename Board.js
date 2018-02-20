a6e376cd95096203e809f028bbe17d20dd09a4b0
Santiago Iraola <iraola.santi@gmail.com>
2018-02-20 15:30:36 -0800

    added all checks for a winner

--- Board.js
+++ Board.js
@@ -4,6 +4,7 @@ class Board {
     this.playerOne = new Player('X', 1);
     this.playerTwo = new Player('O', 2);
     this.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
+    this.currentTurn = 0;
   }
 
   printBoard() {
@@ -18,11 +19,24 @@ class Board {
       }
     }
   }
-  checkIfWinner() {
+  checkIfWinner(symbol) {
+    let winner = this.checkMajorDiagonal(symbol) || this.checkMinorDiagonal(symbol);
+    if(winner) {
+      console.log('WINNER IS: PLAYER ', symbol);
+      return true;
+    }
+
+    for(let i = 0; i < 3; i++) {
+      if(this.checkColumn(symbol, i) || this.checkRow(symbol, i)) {
+        console.log('WINNER IS: PLAYER ', symbol);
+        return true;
+      };
+    }
+    return false;
   }
 
-  checkMajorDiagonal() {
-    let symbolToCheck = this.board[0][0] === 'X' || this.board[0][0] === 'O' ? this.board[0][0] : null;
+  checkMajorDiagonal(symbol) {
+    let symbolToCheck = this.board[0][0] === symbol ? this.board[0][0] : null;
     console.log('s', symbolToCheck);
     if(this.board[1][1] === symbolToCheck) {
       if(this.board[2][2] === symbolToCheck) {
@@ -32,9 +46,8 @@ class Board {
     return false;
   }
 
-  checkMinorDiagonal() {
-    let symbolToCheck = this.board[0][2] === 'X' || this.board[0][2] === 'O' ? this.board[0][2] : null;
-    console.log('s', symbolToCheck);
+  checkMinorDiagonal(symbol) {
+    let symbolToCheck = this.board[0][2] === symbol ? this.board[0][2] : null;
     if(this.board[1][1] === symbolToCheck) {
       if(this.board[2][0] === symbolToCheck) {
         return true;
@@ -42,24 +55,37 @@ class Board {
     }
     return false;
   }
-  checkRow(rowIndex) {
-    let symbolToCheck = this.board[rowIndex][0] === 'X' || this.board[rowIndex][0] === 'O' ? this.board[0][2] : null;
-    if(this.board[1][1] === symbolToCheck) {
-      if(this.board[2][0] === symbolToCheck) {
+  checkRow(symbol, rowIndex) {
+    let symbolToCheck = this.board[rowIndex][0] === symbol ? this.board[rowIndex][0] : null;
+    if(this.board[rowIndex][1] === symbolToCheck) {
+      if(this.board[rowIndex][2] === symbolToCheck) {
         return true;
       }
     }
     return false;
   }
-  checkColumn(columnIndex) {
+
+  checkColumn(symbol, columnIndex) {
+    let symbolToCheck = this.board[0][columnIndex] === symbol ? this.board[0][columnIndex] : null;
+    if(this.board[1][columnIndex] === symbolToCheck) {
+      if(this.board[2][columnIndex] === symbolToCheck) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  startTurn() {
+
+  }
+  startGame() {
 
   }
 }
 
 let b = new Board();
-b.board[0][2] = 'X';
+b.board[0][0] = 'X';
 b.board[1][1] = 'X';
-b.board[2][0] = 'X';
-console.log(b.checkMinorDiagonal());
-
+b.board[2][2] = 'X';
 b.printBoard();
+b.checkIfWinner('X');
